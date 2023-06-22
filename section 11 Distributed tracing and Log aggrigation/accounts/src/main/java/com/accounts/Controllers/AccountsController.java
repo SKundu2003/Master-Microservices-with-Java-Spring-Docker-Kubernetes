80937144199061b71b733bcfe70063a58c1fddf2
Souvik Kundu <kundu9831562286@gmail.com>
2023-06-22 14:34:30 +0530

    implimented distributed tracing using micrometer tracing

--- section 11 Distributed tracing and Log aggrigation/accounts/src/main/java/com/accounts/Controllers/AccountsController.java
+++ section 11 Distributed tracing and Log aggrigation/accounts/src/main/java/com/accounts/Controllers/AccountsController.java
@@ -8,6 +8,7 @@ import com.accounts.Repositories.AccountsRepository;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 public class AccountsController {
@@ -49,10 +51,17 @@ public class AccountsController {
     @Autowired
     LoansFeignClient loansFeignClient;
 
+
+//    ####################################################################################################################
+//    Logger object creation
+    private static final Logger Logger = (java.util.logging.Logger) LoggerFactory.getLogger(AccountsController.class);
     @GetMapping("/getAllDetails")
 //  to implement retry pattern
     @Retry(name = "retryForCustomerDetails",fallbackMethod = "myCustomerDetailsFallBack")
     public CustomerDetails getData(@RequestBody Customer customer){
+//        Logging the data
+        Logger.info("Inside the accounts service");
+
         Accounts accounts = accountsRepository.findAccountsByCustomerId(customer.getCustomerId());
         List<Cards> cards = cardsFeignClient.getCardsDetails(customer);
         List<Loans> loans = loansFeignClient.getAllloans(customer);
@@ -60,6 +69,10 @@ public class AccountsController {
         customerDetails.setAccounts(accounts);
         customerDetails.setCards(cards);
         customerDetails.setLoans(loans);
+
+
+//        Logging the data
+        Logger.info("Data is : "+customerDetails.toString());
         return customerDetails;
     }
 
--- section 11 Distributed tracing and Log aggrigation/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ section 11 Distributed tracing and Log aggrigation/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -4,6 +4,7 @@
 package com.eazybytes.cards.controller;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,9 +27,17 @@ public class CardsController {
 	@Autowired
 	private CardsRepository cardsRepository;
 
+//	Implementing the logger
+	private static final Logger log = Logger.getLogger(String.valueOf(CardsController.class));
+
 	@PostMapping("/myCards")
 	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+//		showing the log
+		log.info("Inside the cards service");
+
 		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+
+		log.info("method ended");
 		if (cards != null) {
 			return cards;
 		} else {
