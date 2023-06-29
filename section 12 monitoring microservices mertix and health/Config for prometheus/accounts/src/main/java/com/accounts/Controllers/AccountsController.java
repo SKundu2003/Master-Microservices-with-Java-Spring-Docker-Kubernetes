37bd5a3643b9881a6c6ac71816701bf2c3af135b
Souvik Kundu <kundu9831562286@gmail.com>
2023-06-29 13:10:26 +0530

    Changes

--- section 12 monitoring microservices mertix and health/Config for prometheus/accounts/src/main/java/com/accounts/Controllers/AccountsController.java
+++ section 12 monitoring microservices mertix and health/Config for prometheus/accounts/src/main/java/com/accounts/Controllers/AccountsController.java
@@ -8,6 +8,7 @@ import com.accounts.Repositories.AccountsRepository;
 
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.micrometer.core.annotation.Timed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,8 @@ public class AccountsController {
     @Autowired
     private TestShowingAllConfigData testShowingAllConfigData;
     @PostMapping("/myAccount")
+//    It will tell my matrix the time taken to execute this method
+    @Timed(value = "getAccountDetails.time",description = "Time taken to return the account details")
     public Accounts getAccountDetails(@RequestBody Customer customer){
         Accounts accounts = accountsRepository.findAccountsByCustomerId(customer.getCustomerId());
         return accounts;
